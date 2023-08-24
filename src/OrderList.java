869aac555e2404ed31f67a88b73038184de45e0b
ColinCPittman <colincpittman@gmail.com>
2023-08-24 11:53:14 -0400

    WIP

--- src/OrderList.java
+++ src/OrderList.java
@@ -1,16 +1,35 @@
 public class OrderList {
-    private Order[] orders = ;
+    private Order[] orders = new Order[3];
+
     public OrderList() {
         orders = new Order[3];
     }
-    public void addOrder(Order order) {
-        for (int i = 0; i < orders.length; i++) {
-            if(orders[i] == null) {
-                orders[i] = order;
-                return;
-            }
-            Order newOrders = new Order[orders.length *2];
 
+    public void addOrder(Order order) {
+        int nextEmptySlot = findNextEmptySlot();
+            if (nextEmptySlot != -1) {
+                orders[nextEmptySlot] = order;
+            }
+            else {
+                doubleSizeOfOrderArray();
+                addOrder(order);
+            }
+    }
+
+    private void doubleSizeOfOrderArray() { //doubles the size of the provided Order array
+        Order[] newOrders = new Order[orders.length * 2];
+        for (int j = 0; j < orders.length; j++) {
+            newOrders[j] = orders[j];
         }
+        orders = newOrders;
+    }
+
+    private int findNextEmptySlot() { //returns index of first empty slot, or negative -1 if there are none
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] == null) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
