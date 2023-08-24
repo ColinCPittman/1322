1de867871e9841553bcae34048ae20b08748192a
ColinCPittman <colincpittman@gmail.com>
2023-08-24 14:48:25 -0400

    WIP2

--- src/OrderList.java
+++ src/OrderList.java
@@ -15,11 +15,48 @@ public class OrderList {
                 addOrder(order);
             }
     }
+    public void readyOrder(int ID) {
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i].getID() == ID) {
+                orders[i].ready = true;
+            }
+        }
+    }
+    private void SortOrders() {
+        shiftNullsToEnd();
+        insertionSortUntilNullReached();
+    }
+
+    private void insertionSortUntilNullReached() {
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] == null) break;
+            Order key = orders[i];
+            int position = i;
+            while (position > 0 && key.getID() < orders[position - 1].getID()) {
+                orders[position] = orders[position - 1];
+                position--;
+            }
+            orders[position] = key;
+        }
+    }
+
+    private void shiftNullsToEnd() {
+        int nonNullCount = 0;
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] != null) {
+                orders[nonNullCount++] = orders[i];
+            }
+        }
+        while (nonNullCount < orders.length) {
+            orders[nonNullCount++] = null;
+        }
+    }
+
 
     private void doubleSizeOfOrderArray() { //doubles the size of the provided Order array
         Order[] newOrders = new Order[orders.length * 2];
-        for (int j = 0; j < orders.length; j++) {
-            newOrders[j] = orders[j];
+        for (int i = 0; i < orders.length; i++) {
+            newOrders[i] = orders[i];
         }
         orders = newOrders;
     }
